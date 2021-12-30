4d875a99679b2af2016adbc45613d34f1ffb2e74
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-30 10:14:17 -0300

    ee

--- Sources/NavigationBar/NavigationBar.swift
+++ Sources/NavigationBar/NavigationBar.swift
@@ -1,6 +1,21 @@
-public struct NavigationBar {
-    public private(set) var text = "Hello, World!"
+#if !os(macOS)
+import UIKit
+import SwiftUI
 
-    public init() {
+public struct NavBarAppearance: ViewModifier {
+    public init(backgroundColor: UIColor, tintColor: UIColor) {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = backgroundColor
+        appearance.titleTextAttributes = [.foregroundColor: tintColor]
+        appearance.largeTitleTextAttributes = [.foregroundColor: tintColor]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().tintColor = tintColor
+    }
+    
+    public func body(content: Content) -> some View {
+        content
     }
 }
+#endif
